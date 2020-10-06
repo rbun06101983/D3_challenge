871276f37b8430bf8cb6fd49e9c5939d62a4961a
rbun06101983 <65375673+rbun06101983@users.noreply.github.com>
2020-10-06 17:21:05 -0600

    updated app.js file

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -68,10 +68,64 @@ d3.csv("assets/data/data.csv").then(function(health_data){
      .attr('class', 'stateCircle')
      .attr('stroke', 'black');
 
-     //Initializing tooltip in the chart
-     var toolTip=d3.tip()
+    //Initializing tooltip in the chart
+    var toolTip=d3.tip()
      .attr('class', 'd3-tip')
      .offset([0,0])
-     .html(function(d))
-    
-})
+     .html(function(d){
+        return (`<strong>${d.state}</br></br>Lacks Healthcare (%):</br>${d.healthcare}</br>Poverty (%):</br>${d.poverty}<strong>`);
+     });
+
+     //create tooltip in chart
+    svg.call(toolTip);
+     
+    //create event listeners to display and hide the tooltip
+    circleGroups.on("click", function(data){
+        toolTip.show(data, this);
+    });
+    //onmouseover event
+    circleGroups.on("mouseover", function(data){
+         toolTip.show(data, this);
+    });
+    //onmouseout event
+    circleGroups.on("mouseout", function(data){
+         toolTip.hide(data, this);
+    });
+
+    //Create axes label
+    chartGroup.append('text')
+     .attr('transform', "rotate(-90)")
+     .attr('y', 0-margin.left+40)
+     .attr('x', 0-(chartHeight/2))
+     .attr('dy', "1em")
+     .attr('class', 'axisText')
+     .text("Lacks Healthcare (%")
+
+    chartGroup.append('text')
+     .attr('transform', `translate(${chartWidth/2}, ${chartHeight+margin.top+30})`)
+     .attr('class','axisText')
+     .text('Poverty (%')
+
+    //State Abbreviations in circles
+    chartGroup.append('text')
+     .attr('class', 'stateText')
+     .style('font-size', '10px')
+     .style('font-weight', 'bold')
+     .selectAll('tspan')
+     .data(health_data)
+     .enter()
+     .append('tspan')
+     .attr('x', function(data){
+        return xLinearScale(data.poverty);
+     })
+     .attr('y', function(data){
+        return yLinearScale(data.healthcare -0.2);
+     })
+     .text(function(data){
+        return data.abbr
+    });
+
+}).catch(function(error){
+   console.log(error);
+
+});
